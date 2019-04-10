c022de7989821a2dfc3fc4d99ed4a3cd605ab781
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-10 16:09:36 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -56,8 +56,8 @@ def runner
   card_total = initial_round
   until card_total >= 21
     hit?(card_total)
-    display_card_total(card_total)
+    card_total = display_card_total(card_total)
   end
-  end_game
+  end_game(card_total)
 end
     
